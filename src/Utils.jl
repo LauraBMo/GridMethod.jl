99f436cc39f77960aa6c09b20d8fe07a93d0493f
Laura Brustenga i Moncusí <brust@math.ku.dk>
2023-01-19 10:02:20 +0100

    pushing changes to Utils.jl

--- src/Utils.jl
+++ src/Utils.jl
@@ -3,14 +3,14 @@
 """
     rand_poly(::Type{T}=Float64, X, d; coeffs = I -> randn(T), n = length(X))
 
-Returns a random polynomial with coefficients of type `T` in variables `X` with degree `d`.
+Returns a random polynomial of coefficient-type `T` in variables `X` with degree `d`.
 
 # Keyword arguments:
  - `coeffs`: A function that takes a tuple of exponents and returns a coefficient.
  - `n`: The number of variables.
 """
 function rand_poly(::Type{T}, X, d; coeffs = I -> randn(T), n = length(X)) where T
-    sum(I -> coeffs(I)*_monomial(X, I), Combinatorics.multiexponents(n, d))
+    sum(I -> coeffs(I)*monomial(X, I), Combinatorics.multiexponents(n, d))
 end
 rand_poly(X, d; coeffs = I -> randn(Float64), n = length(X)) =
     rand_poly(Float64, X, d; coeffs = coeffs, n = n)
@@ -18,20 +18,20 @@ rand_poly(X, d; coeffs = I -> randn(Float64), n = length(X)) =
 const ε = 1e-6 # bound for meaning zero
 function last_sval(M, ε)
     U, svals, Vt = LA.svd(M; alg = QRIteration())
-    # svals = LA.svdvals(M; alg = QRIteration())
+    # svals = LA.svdvals(M)
     return svals[findlast(>(ε), svals)]
 end
 
 ## From HC.jl
 """
-    _monomial(X, I, n = length(X))
+    monomial(X, I, n = length(X))
 
 Returns the monomial of variables `X` with exponents `I`.
 
 # Keyword arguments:
  - `n`: The number of variables.
 """
-function _monomial(X, I, n=length(X))
+function monomial(X, I, n=length(X))
     prod(i -> X[i]^I[i], 1:n)
 end
 
@@ -43,15 +43,15 @@ Returns the identity matrix of size `n` and type `T`.
 function Id(::Type{T}, n) where T
     LA.Diagonal(ones(T, n))
 end
-Id(n) = Id(Float64, n)
+Id(v::AbstractVector) = Id(eltype(v), length(v))
 # diagonal(v) = cat(v...; dims = (1, 2)) # Without LinearAlgebra.jl
 
 """
-    _norm(v, p = 2)
+    p_norm(v, p = 2)
 
 Returns the p-norm of a vector `v`.
 """
-function _norm(v, p = 2)
+function p_norm(v, p = 2)
     sum(x -> x^p, v)^(1/p)
 end
 
@@ -61,19 +61,19 @@ end
 Returns the degree matrix of a system of polynomials `F`.
 """
 function Δ(F)
-    LA.Diagonal(_degrees(F))
+    LA.Diagonal(degrees(F))
 end
 
 """
     D(Jx, x, n = length(x))
 
-Returns the matrix `Jx` multiplied by the difference between the identity matrix and the outer product of `x` and its conjugate transpose.
+Returns the matrix difference between the identity matrix and the outer product of `x` and its conjugate transpose.
 
 # Keyword arguments:
  - `n`: The size of the identity matrix.
 """
-function D(Jx, x, n = length(x))
-    Jx*(Id(n) - x*transpose(conj(x)))
+function D(x)
+    Id(x) - x*transpose(conj(x))
 end
 
 # # monomial(X, I) = prod(X.^I)
